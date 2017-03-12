ca471fa2d636a4f8fe45db30b4de8c365f9a8d64
Tom Power <tpower21@gmail.com>
2017-03-12 13:41:25 +0000

    add maven sample

---
+++ spring-boot-flyway-migrate-maven-sample/src/main/java/com/github/tom/power/spring/boot/flyway/migrate/sample/SpringBootFlywayMigrateSampleApplication.java
@@ -0,0 +1,12 @@
+package com.github.tom.power.spring.boot.flyway.migrate.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFlywayMigrateSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootFlywayMigrateSampleApplication.class, args);
+    }
+}
